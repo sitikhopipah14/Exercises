aac5d5f544c47552e1161e82e27e82e74a686fb8
Sri Rizki Batu Bara <sririzki.02062000@gmail.com>
2021-10-01 15:51:27 +0700

    initial commmit

---
+++ app/src/main/java/com/example/exercises/vidioexercise_activity.java
@@ -0,0 +1,14 @@
+package com.example.exercises;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class vidioexercise_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vidioexercise_main4);
+    }
+}
